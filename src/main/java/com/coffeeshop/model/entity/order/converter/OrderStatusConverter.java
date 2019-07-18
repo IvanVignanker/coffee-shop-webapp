25340cdd0768f40db18160af9afc9d81d5cd87a3
Admin <che.sergii@gmail.com>
2019-07-18 13:47:43 +0300

    Created order and product entities

---
+++ src/main/java/com/coffeeshop/model/entity/order/converter/OrderStatusConverter.java
@@ -0,0 +1,20 @@
+package com.coffeeshop.model.entity.order.converter;
+
+import com.coffeeshop.model.entity.order.status.OrderStatus;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class OrderStatusConverter implements AttributeConverter<OrderStatus, Integer> {
+
+    @Override
+    public Integer convertToDatabaseColumn(OrderStatus orderStatus) {
+        return orderStatus == null ? OrderStatus.UNPROCESSED.getId() : orderStatus.getId();
+    }
+
+    @Override
+    public OrderStatus convertToEntityAttribute(Integer integer) {
+        return integer == null ? OrderStatus.UNPROCESSED : OrderStatus.getById(integer);
+    }
+}
