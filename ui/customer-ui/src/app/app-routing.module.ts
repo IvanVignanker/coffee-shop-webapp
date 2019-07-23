687d83d7978b1dacf5bfa0d2483dc77e309adb66
Admin <che.sergii@gmail.com>
2019-07-23 18:06:47 +0300

    Created footer component

--- ui/customer-ui/src/app/app-routing.module.ts
+++ ui/customer-ui/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductDetailsComponent } from "./product-details/product-details.component";
 import { CheckoutComponent } from "./checkout/checkout.component";
 import { ProductListComponent } from "./product-list/product-list.component";
+import {FooterComponent} from "./footer/footer.component";
 
 const routes: Routes = [
   {path: '', component: ProductListComponent},
   {path: 'productList', component: ProductListComponent},
   {path: 'productDetails/:id', component: ProductDetailsComponent},
   {path: 'checkout', component: CheckoutComponent},
-  {path: 'received/:orderId', component: CheckoutComponent}
+  {path: 'received/:orderId', component: CheckoutComponent},
+  {path: 'footer', component: FooterComponent},
 ];
 
 @NgModule({
