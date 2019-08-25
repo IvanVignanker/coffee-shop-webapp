d67db08f42809759727b4f7ea2681571c2f7656f
Ivan Vignanker <i.vignanker@gmail.com>
2019-08-25 15:02:35 +0300

    created Swagger product list request

--- src/main/java/com/coffeeshop/model/customer/web/productList/ProductListDTORequest.java
+++ src/main/java/com/coffeeshop/model/customer/web/productList/ProductListDTORequest.java
@@ -1,5 +1,6 @@
 package com.coffeeshop.model.customer.web.productList;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @NoArgsConstructor
@@ -8,12 +9,19 @@ import lombok.*;
 @Builder
 public class ProductListDTORequest {
 
+    @ApiModelProperty(example = "1")
     private Long page;
+    @ApiModelProperty(example = "10")
     private Integer results;
+    @ApiModelProperty(example = "Jamaican Arabica")
     private String search;
+    @ApiModelProperty(example = "10")
     private Double minPrice;
+    @ApiModelProperty(example = "100")
     private Double maxPrice;
-    private String sortByDTORequestEnum;
+
+    @ApiModelProperty(example = "popular | price | name")
+    private String sortBy;
 
     ProductCharacteristicsDTORequest productCharacteristicsDTORequest;
 }
