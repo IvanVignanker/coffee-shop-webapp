b9cea6aa8c25e6e2c91b18ec06c1e7a266279ddb
mr.che <che.sergii@gmail.com>
2019-08-25 13:21:13 +0300

    Created Swagger product creation request

--- src/main/java/com/coffeeshop/model/admin/request/ProductCreationRequest.java
+++ src/main/java/com/coffeeshop/model/admin/request/ProductCreationRequest.java
@@ -1,6 +1,7 @@
 package com.coffeeshop.model.admin.request;
 
 import com.coffeeshop.model.customer.entity.product.product.status.ProductType;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
@@ -15,16 +16,27 @@ import javax.validation.constraints.NotNull;
 public class ProductCreationRequest {
 
     @NotBlank
+    @ApiModelProperty(example = "Jamaican Blue Mountain Coffee")
+    private String productName;
+
+    @NotBlank
+    @ApiModelProperty(example = "Jamaica Blue Mountain Coffee is typically wet " +
+            "processed and is very well balanced with a full body.")
     private String shortDescription;
 
     @Length(min = 6, max = 500)
+    @ApiModelProperty(example = "A sophisticated coffee is a wet processed coffee " +
+            "that is silky smooth and well-balanced, " +
+            "with an excellent full body, " +
+            "a classic and refined mild taste with a slightly sweet flavor.")
     private String description;
+
+    @ApiModelProperty(example = "SGVsbG8sIFdvcmxkIQ==")
     private String previewImage;
 
     @Min(value = 1)
     @NotNull
+    @ApiModelProperty(example = "99.99")
     private Double unitPrice;
     private ProductType productCategoryId;
-    private Boolean available;
-    private Integer version;
 }
