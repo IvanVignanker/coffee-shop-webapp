415f1e6477f95785e05065a52bb0b7f29da7c18c
mr.che <che.sergii@gmail.com>
2019-09-08 20:42:16 +0300

    Deleted all mistakes

--- ui/customer-ui/src/app/product-list/search-form/search-form.component.ts
+++ ui/customer-ui/src/app/product-list/search-form/search-form.component.ts
@@ -13,8 +13,6 @@ export class SearchFormComponent implements OnInit {
   searchForm: FormGroup;
 
   search: string;
-  priceMin: number;
-  priceMax: number;
   bitterMin: number;
   bitterMax: number;
   sourMin: number;
