045ba2f1029b98ec84d813510d6011e0333f03fc
Cherepanov Sergey <che.sergii@gmail.com>
2019-09-14 13:02:55 +0300

    Added changes

--- src/main/java/com/coffeeshop/repository/ProductSearchRepositoryCustomImpl.java
+++ src/main/java/com/coffeeshop/repository/ProductSearchRepositoryCustomImpl.java
@@ -1,10 +1,7 @@
 package com.coffeeshop.repository;
 
-import com.coffeeshop.exception.ProductNotFoundException;
 import com.coffeeshop.model.customer.web.productList.*;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -19,8 +16,13 @@ public class ProductSearchRepositoryCustomImpl implements ProductSearchRepositor
 
     @Override
     public ProductListDTOResponse searchProductByName(ProductListDTORequest productListDTORequest) {
-        TypedQuery<Object[]> typedQuery = createQuery(productListDTORequest);
-        List<Object[]> responsesFromDB = typedQuery.getResultList();
+        String query = createQuery();
+        TypedQuery<Object[]> jpaQuery = entityManager.createQuery(query, Object[].class);
+        Map<String, Object> parameters = setParameters(productListDTORequest);
+        for (Map.Entry<String, Object> map : parameters.entrySet()) {
+            jpaQuery.setParameter(map.getKey(), map.getValue());
+        }
+        List<Object[]> responsesFromDB = jpaQuery.getResultList();
         List<ProductDTOResponse> productDTOResponses = new ArrayList<>();
 
         for (Object[] list : responsesFromDB) {
@@ -47,150 +49,37 @@ public class ProductSearchRepositoryCustomImpl implements ProductSearchRepositor
                 .build();
     }
 
-    public TypedQuery<Object[]> createQuery(ProductListDTORequest productListDTORequest) {
-        StringBuilder query = new StringBuilder();
+    public Map<String, Object> setParameters(ProductListDTORequest productListDTORequest) {
         Map<String, Object> parameters = new HashMap<>();
+        parameters.put("search", productListDTORequest.getSearch()+"%");
+        parameters.put("priceMin",productListDTORequest.getPriceMin());
+        parameters.put("priceMax",productListDTORequest.getPriceMax());
+        parameters.put("bitterFrom",productListDTORequest.getCharacteristics().getBitterFrom());
+        parameters.put("bitterTo",productListDTORequest.getCharacteristics().getBitterTo());
+        parameters.put("sourFrom",productListDTORequest.getCharacteristics().getSourFrom());
+        parameters.put("sourTo",productListDTORequest.getCharacteristics().getSourTo());
+        parameters.put("strongFrom",productListDTORequest.getCharacteristics().getStrongFrom());
+        parameters.put("strongTo",productListDTORequest.getCharacteristics().getStrongTo());
+        parameters.put("decaf",productListDTORequest.getCharacteristics().getDecaf());
+        parameters.put("ground",productListDTORequest.getCharacteristics().getGround());
+        return parameters;
+    }
 
+    public String createQuery() {
+        StringBuilder query = new StringBuilder();
         query.append("select p.id, p.productName, p.shortDescription, p.productCategoryId, p.previewImage, p.unitPrice, ");
         query.append("pc.bitter, pc.sour, pc.strong, pc.decaf, ");
         query.append("pq.quantity ");
-        query.append("from Product p");
-        query.append(" join ProductCoffee pc on p.id=pc.product.id");
-        query.append(" join ProductQuantity pq on p.id=pq.product.id");
-
-
-        if (!productListDTORequest.getSearch().equals(null) || !productListDTORequest.getPriceMax().equals(null) && !productListDTORequest.getPriceMax().equals(null)) {
-            if (!productListDTORequest.getSearch().equals(null)) {
-                query.append(" where p.productName like :search");
-                parameters.put("search", productListDTORequest.getSearch()+"%");
-                if (!productListDTORequest.getPriceMin().equals(null) && !productListDTORequest.getPriceMax().equals(null)) {
-                    query.append(" and p.unitPrice between :priceMin and :priceMax");
-                    parameters.put("priceMin",productListDTORequest.getPriceMin());
-                    parameters.put("priceMax",productListDTORequest.getPriceMax());
-                }
-            }
-            else query.append(" where p.unitPrice between :priceMin and :priceMax");
-            parameters.put("priceMin",productListDTORequest.getPriceMin());
-            parameters.put("priceMax",productListDTORequest.getPriceMax());
-        }
-        if (!productListDTORequest.getCharacteristics().getBitterFrom().equals(null) && !productListDTORequest.getCharacteristics().getBitterTo().equals(null)
-                || !productListDTORequest.getCharacteristics().getSourFrom().equals(null) && !productListDTORequest.getCharacteristics().getSourTo().equals(null)
-                || !productListDTORequest.getCharacteristics().getStrongFrom().equals(null) && !productListDTORequest.getCharacteristics().getStrongTo().equals(null)
-                || !productListDTORequest.getCharacteristics().getDecaf().equals(null) || !productListDTORequest.getCharacteristics().getGround().equals(null)) {
-
-            if (!productListDTORequest.getCharacteristics().getBitterFrom().equals(null) && !productListDTORequest.getCharacteristics().getBitterTo().equals(null)) {
-                query.append(" and pc.bitter between :bitterFrom and :bitterTo");
-                parameters.put("bitterFrom",productListDTORequest.getCharacteristics().getBitterFrom());
-                parameters.put("bitterTo",productListDTORequest.getCharacteristics().getBitterTo());
-                if (!productListDTORequest.getCharacteristics().getSourFrom().equals(null) && !productListDTORequest.getCharacteristics().getSourTo().equals(null)) {
-                    query.append(" and pc.sour between :sourFrom and :sourTo");
-                    parameters.put("sourFrom",productListDTORequest.getCharacteristics().getSourFrom());
-                    parameters.put("sourTo",productListDTORequest.getCharacteristics().getSourTo());
-                    if (!productListDTORequest.getCharacteristics().getStrongFrom().equals(null) && !productListDTORequest.getCharacteristics().getStrongTo().equals(null)) {
-                        query.append(" and pc.strong between :strongFrom and :strongTo");
-                        parameters.put("strongFrom",productListDTORequest.getCharacteristics().getStrongFrom());
-                        parameters.put("strongTo",productListDTORequest.getCharacteristics().getStrongTo());
-                        if (!productListDTORequest.getCharacteristics().getDecaf().equals(null)) {
-                            query.append(" and pc.decaf = :decaf");
-                            parameters.put("decaf",productListDTORequest.getCharacteristics().getDecaf());
-                            if (!productListDTORequest.getCharacteristics().getGround().equals(null)) {
-                                query.append(" and pc.ground = :ground");
-                                parameters.put("ground",productListDTORequest.getCharacteristics().getGround());
-                            }
-                        }
-                    }
-                }
-                else if (!productListDTORequest.getCharacteristics().getStrongFrom().equals(null) && !productListDTORequest.getCharacteristics().getStrongTo().equals(null)) {
-                    query.append(" and pc.strong between :strongFrom and :strongTo");
-                    parameters.put("strongFrom",productListDTORequest.getCharacteristics().getStrongFrom());
-                    parameters.put("strongTo",productListDTORequest.getCharacteristics().getStrongTo());
-                    if (!productListDTORequest.getCharacteristics().getDecaf().equals(null)) {
-                        query.append(" and pc.decaf = :decaf");
-                        if (!productListDTORequest.getCharacteristics().getGround().equals(null)) {
-                            query.append(" and pc.ground = :ground");
-                            parameters.put("ground",productListDTORequest.getCharacteristics().getGround());
-                        }
-                    }
-                }
-                else if (!productListDTORequest.getCharacteristics().getDecaf().equals(null)) {
-                    query.append(" pc.decaf = :decaf");
-                    parameters.put("decaf",productListDTORequest.getCharacteristics().getDecaf());
-                    if (!productListDTORequest.getCharacteristics().getGround().equals(null)) {
-                        query.append(" and pc.ground = :ground");
-                    }
-                }
-                else if (!productListDTORequest.getCharacteristics().getGround().equals(null)) {
-                    query.append(" and pc.ground = :ground");
-                    parameters.put("ground",productListDTORequest.getCharacteristics().getGround());
-                }
-            }
-            else if (!productListDTORequest.getCharacteristics().getSourFrom().equals(null) && !productListDTORequest.getCharacteristics().getSourTo().equals(null)) {
-                query.append(" pc.sour between :sourFrom and :sourTo");
-                parameters.put("sourFrom",productListDTORequest.getCharacteristics().getSourFrom());
-                parameters.put("sourTo",productListDTORequest.getCharacteristics().getSourTo());
-                if (!productListDTORequest.getCharacteristics().getStrongFrom().equals(null) && !productListDTORequest.getCharacteristics().getStrongTo().equals(null)) {
-                    query.append(" and pc.strong between :strongFrom and :strongTo");
-                    parameters.put("strongFrom",productListDTORequest.getCharacteristics().getStrongFrom());
-                    parameters.put("strongTo",productListDTORequest.getCharacteristics().getStrongTo());
-                    if (!productListDTORequest.getCharacteristics().getDecaf().equals(null)) {
-                        query.append(" and pc.decaf = :decaf");
-                        parameters.put("decaf",productListDTORequest.getCharacteristics().getDecaf());
-                        if (!productListDTORequest.getCharacteristics().getGround().equals(null)) {
-                            query.append(" and pc.ground = :ground");
-                            parameters.put("ground",productListDTORequest.getCharacteristics().getGround());
-                        }
-                    }
-                }
-                else if (!productListDTORequest.getCharacteristics().getDecaf().equals(null)) {
-                    query.append(" and pc.decaf = :decaf");
-                    parameters.put("decaf",productListDTORequest.getCharacteristics().getDecaf());
-                    if (!productListDTORequest.getCharacteristics().getGround().equals(null)) {
-                        query.append(" and pc.ground = :ground");
-                        parameters.put("ground",productListDTORequest.getCharacteristics().getGround());
-                    }
-                }
-                else if (!productListDTORequest.getCharacteristics().getGround().equals(null)) {
-                    query.append(" and pc.ground = :ground");
-                    parameters.put("ground",productListDTORequest.getCharacteristics().getGround());
-                }
-            }
-            else if (!productListDTORequest.getCharacteristics().getStrongFrom().equals(null) && !productListDTORequest.getCharacteristics().getStrongTo().equals(null)) {
-                query.append(" and pc.strong between :strongFrom and :strongTo");
-                parameters.put("strongFrom",productListDTORequest.getCharacteristics().getStrongFrom());
-                parameters.put("strongTo",productListDTORequest.getCharacteristics().getStrongTo());
-                if (!productListDTORequest.getCharacteristics().getDecaf().equals(null)) {
-                    query.append(" and pc.decaf = :decaf");
-                    if (!productListDTORequest.getCharacteristics().getGround().equals(null)) {
-                        query.append(" and pc.ground = :ground");
-                        parameters.put("ground",productListDTORequest.getCharacteristics().getGround());
-                    }
-                }
-                else if (!productListDTORequest.getCharacteristics().getGround().equals(null)) {
-                    query.append(" and pc.ground = :ground");
-                    parameters.put("ground",productListDTORequest.getCharacteristics().getGround());
-                }
-            }
-            else if (!productListDTORequest.getCharacteristics().getDecaf().equals(null)) {
-                query.append(" pc.decaf = :decaf");
-                parameters.put("decaf",productListDTORequest.getCharacteristics().getDecaf());
-                if (!productListDTORequest.getCharacteristics().getGround().equals(null)) {
-                    query.append(" and pc.ground = :ground");
-                    parameters.put("ground",productListDTORequest.getCharacteristics().getGround());
-                }
-            }
-            else if (!productListDTORequest.getCharacteristics().getGround().equals(null)) {
-                query.append(" pc.ground = :ground");
-                parameters.put("ground",productListDTORequest.getCharacteristics().getGround());
-            }
-        }
-        else {
-            query.append(" where 1==1");
-        }
-
-        TypedQuery<Object[]> jpaQuery = entityManager.createQuery(query.toString(), Object[].class);
-        for (Map.Entry<String, Object> map : parameters.entrySet()) {
-            jpaQuery.setParameter(map.getKey(), map.getValue());
-        }
-        return jpaQuery;
+        query.append("from Product p ");
+        query.append("join ProductCoffee pc on p.id=pc.product.id ");
+        query.append("join ProductQuantity pq on p.id=pq.product.id ");
+        query.append("where p.productName like :search ");
+        query.append("and p.unitPrice between :priceMin and :priceMax ");
+        query.append("and pc.bitter between :bitterFrom and :bitterTo ");
+        query.append("and pc.sour between :sourFrom and :sourTo ");
+        query.append("and pc.strong between :strongFrom and :strongTo ");
+        query.append("and pc.decaf = :decaf ");
+        query.append("and pc.ground = :ground");
+        return query.toString();
     }
 }
